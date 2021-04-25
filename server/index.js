74ecf844463b2a0d5f75a043f8cf8b5cb604aebf
Amar <codeking4u@gmail.com>
2021-04-26 00:28:28 +0530

    db creation and post request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const pool = require("./db");
+
 const app = express();
 
 //middleware
@@ -8,6 +10,30 @@ app.use(cors());
 
 //access to request.body
 app.use(express.json());
+
+// ROUTES //
+
+//create
+app.post("/todos", async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *", 
+            [description]
+        );
+
+        res.json(newTodo.rows[0]);
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+//fetch
+
+//update
+
+//delete
+
 
 app.listen(5000, () => {
     console.log('Server running');
