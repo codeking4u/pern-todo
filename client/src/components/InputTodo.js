c6f0c4791ddeea758d79f973d2842507cbc17920
Amar <codeking4u@gmail.com>
2021-04-30 16:29:20 +0530

    client side setup+ gitignore

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const InputTodo = () => {
+    return (
+        <Fragment>
+            <h1>Todo app </h1>
+
+        </Fragment>
+        
+    )
+}
+
+export default InputTodo;
